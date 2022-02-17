48645691729997f3ab03a77951b8e2ec72c8c9d1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-16 22:44:27 -0800

    Current working status

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
   getAllQuotes()
 })
 
+let quoteArr = []
+let likeId = 2
+let created = 1558524358
 const quoteList = document.querySelector('ul#quote-list')
 
 let getAllQuotes = () => {
   fetch('http://localhost:3000/quotes?_embed=likes')
     .then(res => res.json())
-    .then(quotes => quotes.forEach(renderEachQuote))
+    .then(quotes => quoteArr = [...quotes])
+    .then(quotes => quotes.forEach(quote => renderEachQuote(quote)))
+  // .then(quotes => console.log(quotes))
 }
+
 
 /************* HELPER FUNCTIONS *************/
 let makeEl = el => document.createElement(el)
@@ -23,29 +29,68 @@ let renderEachQuote = (input) => {
   const p = makeEl('p')
   const br = makeEl('br')
   const footer = makeEl('footer')
-  const span = makeEl('span')
   const btnSuc = makeEl('button')
   const btnDan = makeEl('button')
 
-  //---------- SET CLASS TO EACH ELEMENT /-----------/
+  card.id = input.id
+
+  //---------- SET CLASS TO EACH ELEMENT /-----------//
   setClass(card, 'quote-card')
   setClass(blockquote, 'blockquote')
   setClass(p, 'mb-0')
   setClass(footer, 'blockquote-footer')
   setClass(btnSuc, 'btn-success')
   setClass(btnDan, 'btn-danger')
-  
+
+  //------- SET TEXTCONTENT TO EACH ELEMENT --------//
   setContent(p, input.quote)
   setContent(footer, input.author)
   setContent(btnDan, 'Delete')
-  setContent(span, input.id)
 
-  btnSuc.innerText = `Likes: ${span}`
-  btnSuc.textContent = 'Likes:'
+  btnSuc.innerHTML = `Likes: <span>${input.likes.length}</span>`  
 
-  // btnSuc.addEventListener('click', )
+  //----------- HANDLE SUCCESS BUTTON --------------// 
+  btnSuc.addEventListener('click', (e) => {
+    let times = e.target.textContent.split(' ')
+    times[1] = Number(times[1]) + 1
+    btnSuc.innerHTML = `Likes: <span>${times[1]}</span>`
 
+    let incrementLikeId = () => likeId += 1
+    let incrementRandNum = () => created += randNum()
+    let randNum = () => Math.floor(Math.random() * (100 - 1) + 1)
 
+    let likeObj = {
+      id: incrementLikeId(),
+      quotedId: input.id,
+      createdAt: incrementRandNum()
+    }
+    input.likes.push(likeObj)
+    // updateBackEnd(input.likes)
+    console.log(input.likes)
+  })
+
+  //------------ HANDLE DELETE BUTTON ---------------//
+  btnDan.addEventListener('click', (e) => { 
+    e.target.parentNode.remove() 
+    const id = e.target.parentNode.id 
+    // console.log('quoteArr before update:', quoteArr); 
+    
+    quoteArr = quoteArr.filter(quote => {
+      quote.id != id    
+    }) 
+    // console.log('quoteArr after update:', quoteArr);    
+  }) 
   card.append(blockquote, p, footer, br, btnSuc, btnDan)
   quoteList.appendChild(card)
+}
+
+function updateRemoveList(item) {
+  fetch(`http://localhost:3000/quotes/${item.id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
 }
